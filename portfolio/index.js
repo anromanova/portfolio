94aad913636988d2f4a3e682b5012a8b17b291cc
Anastasia Romanova <anastasiya.erohina@gmail.com>
2022-01-22 11:55:01 +0000

    feat: add adoptive menu

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,3 +1,20 @@
 console.log(
-    "(10/10) Вёрстка валидная \n(20/20) Вёрстка семантическая \n(42/48)Вёрстка соответствует макету (в footer блок с годом съехал)\n(12/12)Требования к css \n(20/20)Интерактивность, реализуемая через css\n(104/110)"
+    "Привет! :) \n(45/48) Вёрстка соответствует макету. Ширина экрана 768px (частично выполнена секция Video) \n(15/15) Ни на одном из разрешений до 320px включительно не появляется горизонтальная полоса прокрутки. Весь контент страницы при этом сохраняется: не обрезается и не удаляется \n(22/22)На ширине экрана 768рх и меньше реализовано адаптивное меню\n(82/85)=>75\nСпасибо за проверку!"
 )
+
+//Adaptive menu 
+const burger = document.querySelector('.burger');
+const menu = document.querySelector('.nav-list');
+const links = document.querySelectorAll('.nav-link');
+
+burger.addEventListener('click', toggleMenu);
+[...links].forEach((link) => {
+  link.addEventListener('click', toggleMenu);
+})
+
+function toggleMenu() {
+  burger.classList.toggle('open');
+  menu.classList.toggle('nav-list_active');
+  
+}
+burger.addEventListener('click', toggleMenu);
